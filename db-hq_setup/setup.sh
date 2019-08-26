68512807d3764976755bcc24111b9680a044c0a0
Diveyez <nemofbaltimore@gmail.com>
2019-08-26 13:59:34 -0700

    .

--- db-hq_setup/setup.sh
+++ db-hq_setup/setup.sh
@@ -3,4 +3,12 @@
 sudo apt update && sudo apt upgrade -y
 cd ~/
 mkdir coopr-dbhq
-apt install php-7.2+* perl mariadb nginx-full letsencrypt openssh-server
+cd coopr-dbhq
+
+# space for env setup
+
+###### AVOID GLOBAL SERVER RESOURCE USAGE AND ROOT USAGE, BUILD LOCALLY
+
+# apt install php-7.2+* perl mariadb nginx-full letsencrypt openssh-server
+
+# space for moving things
