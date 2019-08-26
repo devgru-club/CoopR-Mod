967f3b3e7568460b4ca19f5733fe938d686b9af0
Diveyez <nemofbaltimore@gmail.com>
2019-08-26 13:55:59 -0700

    .

--- db-hq_setup/setup.sh
+++ db-hq_setup/setup.sh
@@ -1,3 +1,6 @@
-# Install/Setup script for required parts to operate a Database and HQ for CoopR Mod
-
-** Script initially for Linux systems with intention to provide both Windows and MacOS versions**
+#!/bin/bash
+#
+sudo apt update && sudo apt upgrade -y
+cd ~/
+mkdir coopr-dbhq
+apt install
